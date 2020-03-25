adf6125ae61608173a92243b233a9e68a0080f03
Daniel Munoz <ataricoder@gmail.com>
2020-03-25 15:45:28 -0500

    reverted, fixed webpack issue

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import Layout from "@components/layout";
+import Layout from "@components/Layout";
 
 export default function Post() {
 	const router = useRouter();
