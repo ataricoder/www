0f8d9cee29a6bfdf260065928da8bf14c03bae15
Daniel Munoz <ataricoder@gmail.com>
2020-03-25 16:03:31 -0500

    replaced layout with page component

--- pages/post.js
+++ pages/post.js
@@ -1,14 +1,14 @@
 import { useRouter } from "next/router";
-import Layout from "@components/Layout";
+import Page from "@components/page";
 
 const Page = () => {
 	const router = useRouter();
 
 	return (
-		<Layout>
+		<Page>
 			<h1>{router.query.title}</h1>
 			<p>This is the blog post content.</p>
-		</Layout>
+		</Page>
 	);
 };
 
