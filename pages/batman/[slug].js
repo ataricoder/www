198c4033f744076724b98791fb9c6ec0f264ae27
Daniel Munoz <ataricoder@gmail.com>
2020-03-25 19:03:38 -0500

    finalized batman api fetching

--- pages/batman/[slug].js
+++ pages/batman/[slug].js
@@ -1,13 +1,30 @@
-import { useRouter } from "next/router";
 import Page from "@components/page";
+import fetch from "isomorphic-unfetch";
 
-export default function Show() {
-	const router = useRouter();
+const Show = props => (
+	<Page>
+		<h1>{props.show.name}</h1>
+		<p>{props.show.summary.replace(/<[/]?[pb]>/g, "")}</p>
+		{props.show.image ? <img src={props.show.image.medium} /> : null}
+	</Page>
+);
 
-	return (
-		<Page>
-			<h1>{router.query.slug}</h1>
-			<p>This is the show post content.</p>
-		</Page>
-	);
-}
+Show.getInitialProps = async function(context) {
+	// const { id } = context.query;
+
+	// Get the id of the show from the query as an object
+	const id = context.query;
+	console.log(id.slug);
+
+	// Fetch the URL from api and get the response as a JSON object
+	// const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+	const res = await fetch(`https://api.tvmaze.com/shows/` + id.slug);
+	const show = await res.json();
+	console.log(show);
+
+	console.log(`Fetched show: ${show.name}`);
+
+	return { show };
+};
+
+export default Show;
