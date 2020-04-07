4451704e92858b0081660f33b622a48005c0b1b6
Daniel Munoz <ataricoder@gmail.com>
2020-04-07 18:42:04 -0500

    added grid column for responsive

--- components/header/header.js
+++ components/header/header.js
@@ -10,6 +10,15 @@ const Header = () => {
 	const bat = router.pathname == "/batman" ? styles.active : "";
 	const posts = router.pathname == "/post-list" ? styles.active : "";
 
+	function myFunction() {
+		var x = document.getElementById("mobileGroup");
+		if (x.className === "mobileGroup") {
+			x.className += " responsive";
+		} else {
+			x.className = "mobileGroup";
+		}
+	}
+
 	return (
 		<nav className={styles.navbar}>
 			<div className={styles.group}>
@@ -31,19 +40,6 @@ const Header = () => {
 				<Link href="/post-list">
 					<a className={styles.link + " " + posts} title="Posts Page">
 						Posts
-					</a>
-				</Link>
-			</div>
-
-			<div className={styles.mobileGroup}>
-				<Link href="/">
-					<a className={styles.link + " " + home} title="Home Page">
-						Home
-					</a>
-				</Link>
-				<Link href="/about">
-					<a className={styles.link + " " + about} title="About Page">
-						About
 					</a>
 				</Link>
 			</div>
