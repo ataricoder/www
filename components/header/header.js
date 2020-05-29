c65e552a1662952f6d4e4f3806fad0b855e29c1c
Daniel Munoz <ataricoder@gmail.com>
2020-05-28 21:25:28 -0500

    styled a lil bit

--- components/header/header.js
+++ components/header/header.js
@@ -131,7 +131,7 @@ const Header = () => {
 				</div>
 			</Desktop>
 			<Mobile>
-				<Navbar bg="light" expand="lg">
+				<Navbar bg="light" expand="lg" fixed="top">
 					<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
