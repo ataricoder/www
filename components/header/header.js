b5130867b937ab3e34ecc2fd241a7e756b4914ff
Daniel Munoz <ataricoder@gmail.com>
2020-05-28 21:29:56 -0500

    diversified css modules

--- components/header/header.js
+++ components/header/header.js
@@ -11,7 +11,8 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 // From CSS Modules
-import styles from "./header.module.css";
+import desktop from "./desktop.module.css";
+import mobile from "./mobile.module.css";
 // From Media Query for Component Modularization
 import { useMediaQuery } from "react-responsive";
 
@@ -40,21 +41,22 @@ const Header = () => {
 			}
 		});
 	});
-	const scrolled = scroll == true ? styles.scrolled : "";
+	const scrolled = scroll == true ? desktop.scrolled : "";
 
 	// MARK: Handle router and apply custom class with ternary operator
 	// useRouter is a hook, therefore can only be called inside the body of a function component
 	// applies the class link_active if the router detects the url
 	const router = useRouter();
 	// Desktop custom active link classes
-	const home = router.pathname == "/" ? styles.link_active : "";
-	const about = router.pathname == "/about" ? styles.link_active : "";
-	const blog = router.pathname == "/blog" ? styles.link_active : "";
-	const music = router.pathname == "/music" ? styles.link_active : "";
-	const projects = router.pathname == "/projects" ? styles.link_active : "";
-	const bookmarks = router.pathname == "/bookmarks" ? styles.link_active : "";
-	const batman = router.pathname == "/batman" ? styles.link_active : "";
-	const posts = router.pathname == "/post-list" ? styles.link_active : "";
+	const home = router.pathname == "/" ? desktop.link_active : "";
+	const about = router.pathname == "/about" ? desktop.link_active : "";
+	const blog = router.pathname == "/blog" ? desktop.link_active : "";
+	const music = router.pathname == "/music" ? desktop.link_active : "";
+	const projects = router.pathname == "/projects" ? desktop.link_active : "";
+	const bookmarks =
+		router.pathname == "/bookmarks" ? desktop.link_active : "";
+	const batman = router.pathname == "/batman" ? desktop.link_active : "";
+	const posts = router.pathname == "/post-list" ? desktop.link_active : "";
 
 	// MARK: Render
 	return (
@@ -77,50 +79,50 @@ const Header = () => {
 						expand="lg"
 						variant="dark"
 						fixed="top"
-						className={styles.navbar + " " + scrolled}
+						className={desktop.navbar + " " + scrolled}
 						// Use bsPrefix for custom class
 					>
 						<Navbar.Brand href="/">👾</Navbar.Brand>
 						<Nav className="ml-auto">
-							<div className={styles.link_group}>
+							<div className={desktop.link_group}>
 								<Nav.Link
 									href="/"
-									className={styles.link + " " + home}
+									className={desktop.link + " " + home}
 									title="Home"
 								>
 									Home
 								</Nav.Link>
 								<Nav.Link
 									href="/about"
-									className={styles.link + " " + about}
+									className={desktop.link + " " + about}
 									title="About"
 								>
 									About
 								</Nav.Link>
 								<Nav.Link
 									href="/blog"
-									className={styles.link + " " + blog}
+									className={desktop.link + " " + blog}
 									title="Blog"
 								>
 									Blog
 								</Nav.Link>
 								<Nav.Link
 									href="/music"
-									className={styles.link + " " + music}
+									className={desktop.link + " " + music}
 									title="Music"
 								>
 									Music
 								</Nav.Link>
 								<Nav.Link
 									href="/projects"
-									className={styles.link + " " + projects}
+									className={desktop.link + " " + projects}
 									title="Projects"
 								>
 									Projects
 								</Nav.Link>
 								<Nav.Link
 									href="/bookmarks"
-									className={styles.link + " " + bookmarks}
+									className={desktop.link + " " + bookmarks}
 									title="Bookmarks"
 								>
 									Bookmarks
@@ -132,7 +134,7 @@ const Header = () => {
 			</Desktop>
 			<Mobile>
 				<Navbar bg="light" expand="lg" fixed="top">
-					<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+					<Navbar.Brand href="/">👾</Navbar.Brand>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="mr-auto">
