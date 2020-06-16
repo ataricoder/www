e59265e63a881d5b6c6909f69b345961eeefc44d
Daniel Munoz <ataricoder@gmail.com>
2020-06-16 12:13:55 -0500

    added sample content to index page

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,65 @@ export default function Blog() {
 				Science and Health & Society at The University of Texas at
 				Austin, as well as working as a software engineer at Clean 'Em.
 			</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+				bibendum, ligula vel imperdiet auctor, velit nisi consectetur
+				magna, nec feugiat augue nisl et est. Etiam feugiat nisi
+				dignissim mi gravida, non faucibus odio volutpat. Suspendisse
+				porta erat a felis feugiat finibus. Suspendisse varius dolor vel
+				tellus fringilla vehicula. Nunc viverra turpis et leo tempor
+				bibendum. In hac habitasse platea dictumst. Duis sit amet libero
+				odio.
+			</p>
+			<p>
+				Ut ut ex fermentum, hendrerit dui in, pretium arcu. Fusce arcu
+				quam, tristique eget nulla in, lacinia interdum libero. Quisque
+				augue libero, feugiat finibus lorem sit amet, fringilla faucibus
+				dolor. Fusce quis imperdiet massa. Phasellus massa justo, varius
+				in eros a, laoreet posuere ipsum. Etiam nec metus quis velit
+				lobortis porttitor. Nam rhoncus egestas sem vitae tempus. Etiam
+				efficitur scelerisque augue, vitae rhoncus ipsum commodo
+				consectetur. Nullam laoreet turpis et est hendrerit hendrerit.
+				Integer viverra tellus sit amet felis faucibus pulvinar. Integer
+				fringilla dapibus turpis, id pellentesque sapien aliquam non.
+				Cras at cursus lorem, et sodales ante. Nam varius magna eget
+				ipsum ullamcorper, ac finibus nulla fermentum. Aliquam eget
+				vehicula lorem, nec auctor dui.
+			</p>
+			<p>
+				Nam faucibus in arcu eu commodo. Donec quis dui sit amet sapien
+				venenatis pretium et maximus metus. Sed sed enim eu massa
+				tincidunt ullamcorper. Ut fringilla volutpat purus id
+				sollicitudin. Integer interdum porttitor ligula sed sagittis.
+				Quisque ac augue ultrices, fringilla nisi ac, ullamcorper quam.
+				Integer lacinia dolor eget mi tincidunt, in rhoncus lectus
+				ultricies. Donec aliquet consectetur ligula. Pellentesque
+				bibendum egestas facilisis. Aliquam at lacinia sem, vitae
+				interdum dui. Nulla nec est arcu. Integer non tortor rutrum,
+				semper arcu non, congue sapien. Vivamus nec dolor sem.
+			</p>
+			<ul>
+				<a href="/about">
+					Vestibulum volutpat elit eget rhoncus rhoncus.
+				</a>
+			</ul>
+			<ul>
+				<a href="/about">
+					Morbi hendrerit sapien eu lectus dictum scelerisque.
+				</a>
+			</ul>
+			<ul>
+				<a href="/about">
+					Quisque sed magna porta, lobortis est molestie, lobortis
+					nisi.
+				</a>
+			</ul>
+			<ul>
+				<a href="/about">
+					Donec volutpat tortor non quam suscipit, vitae ornare dolor
+					gravida.
+				</a>
+			</ul>
 		</Layout>
 	);
 }
