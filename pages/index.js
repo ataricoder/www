b2c78d8f9fb8b4ddffaaa9afbf64ad377bd016c0
Daniel Munoz <ataricoder@gmail.com>
2020-06-16 18:49:24 -0500

    centered main content

--- pages/index.js
+++ pages/index.js
@@ -35,18 +35,6 @@ export default function Blog() {
 				ipsum ullamcorper, ac finibus nulla fermentum. Aliquam eget
 				vehicula lorem, nec auctor dui.
 			</p>
-			<p>
-				Nam faucibus in arcu eu commodo. Donec quis dui sit amet sapien
-				venenatis pretium et maximus metus. Sed sed enim eu massa
-				tincidunt ullamcorper. Ut fringilla volutpat purus id
-				sollicitudin. Integer interdum porttitor ligula sed sagittis.
-				Quisque ac augue ultrices, fringilla nisi ac, ullamcorper quam.
-				Integer lacinia dolor eget mi tincidunt, in rhoncus lectus
-				ultricies. Donec aliquet consectetur ligula. Pellentesque
-				bibendum egestas facilisis. Aliquam at lacinia sem, vitae
-				interdum dui. Nulla nec est arcu. Integer non tortor rutrum,
-				semper arcu non, congue sapien. Vivamus nec dolor sem.
-			</p>
 			<ul>
 				<a href="/about">
 					Vestibulum volutpat elit eget rhoncus rhoncus.
@@ -61,12 +49,6 @@ export default function Blog() {
 				<a href="/about">
 					Quisque sed magna porta, lobortis est molestie, lobortis
 					nisi.
-				</a>
-			</ul>
-			<ul>
-				<a href="/about">
-					Donec volutpat tortor non quam suscipit, vitae ornare dolor
-					gravida.
 				</a>
 			</ul>
 		</Layout>
