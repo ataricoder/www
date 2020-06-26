b792bd7cacc7d36700f0cf8c36cd83df8ff1f63b
Daniel Munoz <ataricoder@gmail.com>
2020-06-26 12:36:08 -0500

    added some content to bookmarks

--- pages/bookmarks.js
+++ pages/bookmarks.js
@@ -3,7 +3,10 @@ import Layout from "@components/layout/layout";
 export default function Bookmarks() {
 	return (
 		<Layout>
-			<p>Bookmarks Page</p>
+			<h1>Bookmarks Page</h1>
+			<h2>Tools</h2>
+			<li>https://title.sh/</li>
+			<h2>Inspiration</h2>
 		</Layout>
 	);
 }
