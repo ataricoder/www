7f293ab199b324c4e18eb9c7dfc1b9946e472005
Daniel Munoz <ataricoder@gmail.com>
2020-06-26 12:31:30 -0500

    added content to index page

--- pages/index.js
+++ pages/index.js
@@ -17,12 +17,57 @@ export default function Blog() {
 				<b>Right now I'm looking for opportunities.</b>
 			</p>
 			<Button href={"/about"}>More about me</Button>
-			<br></br>
+			<hr></hr>
+			<h1>Education</h1>
+			<Experience
+				href="#"
+				company="The University of Texas at Austin"
+				date="August 2016 - August 2020"
+			>
+				<Position title="Computer Science">
+					Important Coursework: Data Structures, Graphics and Computer
+					Visualization, iOS Mobile Computing, Modern Web
+					Applications, Software Engineering
+				</Position>
+				<Position title="Health and Society">
+					Important Coursework: Epidemiology, Data Analysis,
+					Statistics and Probability
+				</Position>
+			</Experience>
+			<Experience
+				href="#"
+				company="The University of Texas at Austin, Extended Campus"
+				date="October 2017 - May 2018"
+			>
+				<Position title="Web Development Coding Bootcamp"></Position>
+			</Experience>
+			<Experience
+				href="#"
+				company="General Assembly"
+				date="May 2018 - August 2018"
+			>
+				<Position title="User Experience"></Position>
+			</Experience>
+			<Experience
+				href="#"
+				company="Codepath"
+				date="January 2019 - May 2019"
+			>
+				<Position title="iOS Development"></Position>
+			</Experience>
+			<Experience
+				href="#"
+				company="Codepath"
+				date="June 2020 - August 2020"
+			>
+				<Position title="Advanced Software Engineering"></Position>
+			</Experience>
+			<hr></hr>
 			<h1>Experience</h1>
 			<Experience
 				href="#"
 				company="Clean 'Em"
-				date="June 2020 - August 2020"
+				date="June 2018 - August 2020"
 			>
 				<Position title="iOS Software Engineer">
 					Created the API needed on the back-end to interact with the
@@ -30,7 +75,13 @@ export default function Blog() {
 					the API. Used JSON web tokens for authentication and session
 					control.
 				</Position>
-				<Position title="Front-end Software Engineer">
+				<Position title="Full Stack Software Engineer">
+					Created the API needed on the back-end to interact with the
+					iOS application and utilized Cocoapods (Alamofire) to use
+					the API. Used JSON web tokens for authentication and session
+					control.
+				</Position>
+				<Position title="Platform Software Engineer Intern">
 					Created the API needed on the back-end to interact with the
 					iOS application and utilized Cocoapods (Alamofire) to use
 					the API. Used JSON web tokens for authentication and session
@@ -39,69 +90,40 @@ export default function Blog() {
 			</Experience>
 			<Experience
 				href="#"
-				company="Clean 'Em"
-				date="June 2020 - August 2020"
+				company="The University of Texas at Austin"
+				date="June 2018 - June 2020"
 			>
-				<Position title="iOS Software Engineer">
-					Created the API needed on the back-end to interact with the
-					iOS application and utilized Cocoapods (Alamofire) to use
-					the API. Used JSON web tokens for authentication and session
-					control.
-				</Position>
-				<Position title="Front-end Software Engineer">
-					Created the API needed on the back-end to interact with the
-					iOS application and utilized Cocoapods (Alamofire) to use
-					the API. Used JSON web tokens for authentication and session
-					control.
+				<Position title="IT Technician">
+					Helped set up Google student front facing computers
+					controlled via Google Admin Console. Helped creating
+					websites to provide quicker common link access. Helped
+					provision multiple new hardware to employees with all needed
+					software and security controls. Helped with ethernet
+					connectivity, as well as firewall features and networking.
 				</Position>
 			</Experience>
-			<li>
-				iOS Software Engineer Intern at Clean 'Em, June 2020 - August
-				2020
-			</li>
-			<li>
-				Full Stack Software Engineer at Clean 'Em, January 2020 - June
-				2020
-			</li>
-			<li>
-				Front-end Software Engineer at Clean 'Em, August 2018 - January
-				2020
-			</li>
-			<li>
-				Platform Software Engineer Intern at Clean 'Em, June 2018 -
-				August 2018
-			</li>
-			<li>
-				IT Technician at The University of Texas at Austin, June 2018 -
-				August 2020
-			</li>
-			<li>
-				Software Engineer Intern at Gardenmania, June 2017 - August 2017
-			</li>
-			<li>
-				Emergency Room Triage Junior Volunteer at Conroe Regional
-				Medical Center, June 2016 - August 2016
-			</li>
-			<li>
-				Computer Science at The University of Texas at Austin, August
-				2016 - August 2020
-			</li>
-			<li>
-				Health and Society at The University of Texas at Austin, August
-				2016 - August 2020
-			</li>
-			<li>
-				Full Stack Coding Bootcamp at The University of Texas at Austin
-				Extended Campus, October 2017 - May 2018
-			</li>
-			<li>User Experience at General Assembly, May 2018 - August 2018</li>
-			<li>iOS Development at Codepath, January 2019 - May 2019</li>
-			<li>
-				Advanced Software Engineering at Codepath, June 2020 - August
-				2020
-			</li>
-			<li>Class Designer at Argodesign, August 2019 - December 2019</li>
-			<br></br>
+			<Experience
+				href="#"
+				company="Argodesign"
+				date="August 2019 - December 2019"
+			>
+				<Position title="Class Designer"></Position>
+			</Experience>
+			<Experience
+				href="#"
+				company="Gardenmania"
+				date="June 2017 - August 2017"
+			>
+				<Position title="Software Engineer Intern"></Position>
+			</Experience>
+			<Experience
+				href="#"
+				company="Conroe Regional Medical Center"
+				date="June 2016 - August 2016"
+			>
+				<Position title="Emergency Room Junior Volunteer"></Position>
+			</Experience>
+			<hr></hr>
 			<h1>Blog</h1>
 			<p>
 				This is where I write down ideas, opinions and more. Usually
