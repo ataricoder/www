ca0fa6c29fe58a77903db858a32c9e7b407a429f
Daniel Munoz <ataricoder@gmail.com>
2020-06-26 11:57:19 -0500

    fixed font color div bug

--- components/experience/position/position.js
+++ components/experience/position/position.js
@@ -3,7 +3,7 @@ import styles from "@components/experience/position/position.module.css";
 const Position = (props) => (
 	<div className={styles.content}>
 		<div className={styles.title}>{props.title}</div>
-		<div>{props.children}</div>
+		<p className={styles.text}>{props.children}</p>
 	</div>
 );
 
