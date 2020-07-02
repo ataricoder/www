3db1843c4d25213e9526503f21ebdb37824e6200
Daniel Munoz <ataricoder@gmail.com>
2020-07-02 14:09:18 -0500

    added music playlist for june

--- pages/music.js
+++ pages/music.js
@@ -1,9 +1,26 @@
 import Layout from "@components/layout/layout";
+import styled from "styled-components";
+
+const Playlist = styled.iframe`
+	border-radius: 8px;
+	border-color: #000000;
+	border-width: 0px;
+`;
 
 export default function Music() {
 	return (
 		<Layout>
-			<p>Music Page</p>
+			<h1>Music Page</h1>
+			<h2>June 2020</h2>
+			<br></br>
+			<Playlist
+				src="https://open.spotify.com/embed/playlist/6iCo3m5036oll1hsG7D4b4"
+				width="100%"
+				height="380"
+				frameborder="0"
+				allowtransparency="true"
+				allow="encrypted-media"
+			></Playlist>
 		</Layout>
 	);
 }
