c0b38b14bc89dff1541ff03c550a293cdd798855
Daniel Munoz <ataricoder@gmail.com>
2020-07-02 15:35:53 -0500

    switched to iframe music

--- pages/music.js
+++ pages/music.js
@@ -1,11 +1,4 @@
 import Layout from "@components/layout/layout";
-import styled from "styled-components";
-
-const Playlist = styled.iframe`
-	border-radius: 8px !important;
-	border-color: #000000 !important;
-	border-width: 0px !important;
-`;
 
 export default function Music() {
 	return (
@@ -13,14 +6,14 @@ export default function Music() {
 			<h1>Music Page</h1>
 			<h2>June 2020</h2>
 			<br></br>
-			<Playlist
+			<iframe
 				src="https://open.spotify.com/embed/playlist/6iCo3m5036oll1hsG7D4b4"
 				width="100%"
 				height="380"
 				frameborder="0"
 				allowtransparency="true"
 				allow="encrypted-media"
-			></Playlist>
+			></iframe>
 		</Layout>
 	);
 }
