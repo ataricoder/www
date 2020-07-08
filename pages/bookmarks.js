ee8c8f980e2b4f50c131d4a30a6ee4772a7a6f1b
Daniel Munoz <ataricoder@gmail.com>
2020-07-08 13:12:10 -0500

    added more bookmarks, modified style

--- pages/bookmarks.js
+++ pages/bookmarks.js
@@ -47,6 +47,62 @@ export default function Bookmarks() {
 				preview="https://dev.evilrabb.it/static/og-image.png"
 				previewBool={true}
 			></Bookmark>
+			<Bookmark
+				title="Guillermo Rauch"
+				description="Guillermo Rauch's blog"
+				href="https://rauchg.com"
+				preview=""
+				previewBool={false}
+			></Bookmark>
+			<Bookmark
+				title="Alex MacCaw"
+				description="JavaScript programmer, O'Reilly author, open source hacker"
+				href="http://alexmaccaw.com"
+				preview=""
+				previewBool={false}
+			></Bookmark>
+			<Bookmark
+				title="Daniel Destefanis"
+				description="Product designer and developer living in San Francisco, working at Discord."
+				href="https://danieldoes.design"
+				preview=""
+				previewBool={false}
+			></Bookmark>
+			<Bookmark
+				title="Ryo Lu"
+				description=""
+				href="https://ryo.lu"
+				preview=""
+				previewBool={false}
+			></Bookmark>
+			<Bookmark
+				title="Amanda Pinsker"
+				description="Amanda Pinsker is a product and interaction designer who cares about making her work smart, elegant, and simple by design."
+				href="https://amandapinsker.com"
+				preview=""
+				previewBool={false}
+			></Bookmark>
+			<Bookmark
+				title="Andy Matuschak"
+				description=""
+				href="https://andymatuschak.org"
+				preview=""
+				previewBool={false}
+			></Bookmark>
+			<Bookmark
+				title="Dustin Curtis"
+				description="Dustin Curtis"
+				href="https://dustincurtis.com"
+				preview=""
+				previewBool={false}
+			></Bookmark>
+			<Bookmark
+				title="S. J. Zhang"
+				description="A Product Designer and technologist. Currently building a design history catalog / graphic editor Form2Shape."
+				href="https://www.s-j-zhang.com"
+				preview="https://uploads-ssl.webflow.com/5d06cf125b8916fdaac3bdb5/5e24ef9f5cbbd620e223c2ac_openGraph2.png"
+				previewBool={true}
+			></Bookmark>
 			<hr></hr>
 			<h2>Tools</h2>
 			<li>https://title.sh/</li>
@@ -54,14 +110,6 @@ export default function Bookmarks() {
 			<li>https://metatags.io/</li>
 			<hr></hr>
 			<h2>Portfolios</h2>
-			<li>https://rauchg.com</li>
-			<li>https://alexmaccaw.com</li>
-			<li>https://danieldoes.design</li>
-			<li>https://ryo.lu</li>
-			<li>https://amandapinsker.com</li>
-			<li>https://andymatuschak.org</li>
-			<li>https://dustincurtis.com</li>
-			<li>https://www.s-j-zhang.com</li>
 			<li>https://www.kerem.co</li>
 			<li>https://leo.im</li>
 			<li>https://chibicode.com</li>
