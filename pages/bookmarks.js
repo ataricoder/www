515b93610cf9165785d15a37e987e508e532a559
Daniel Munoz <ataricoder@gmail.com>
2020-07-08 23:25:29 -0500

    added more bookmarks

--- pages/bookmarks.js
+++ pages/bookmarks.js
@@ -5,6 +5,7 @@ export default function Bookmarks() {
 	return (
 		<Layout>
 			<h1>Bookmarks</h1>
+			<p>Portfolios and product pages that inspire me.</p>
 			<Bookmark
 				title="Brian Lovin"
 				description="Product designer"
@@ -103,26 +104,121 @@ export default function Bookmarks() {
 				preview="https://uploads-ssl.webflow.com/5d06cf125b8916fdaac3bdb5/5e24ef9f5cbbd620e223c2ac_openGraph2.png"
 				previewBool={true}
 			></Bookmark>
+			<Bookmark
+				title="Kerem Suer"
+				description="Kerem is an interdisciplinary designer living in San Francisco, California. He helps startups reimagine, prototype and design products and brands."
+				href="https://www.kerem.co"
+				preview="http://suer.me/img/og.png"
+				previewBool={true}
+			></Bookmark>
+			<Bookmark
+				title="Leo Lamprecht"
+				description="A teenagers view on web development and intuitive UI design."
+				href="https://www.kerem.co"
+				preview=""
+				previewBool={false}
+			></Bookmark>
+			<Bookmark
+				title="Shu Uesugi"
+				description="👞 @chibicode 🥃"
+				href="https://chibicode.com"
+				preview="https://chibicode.com/images/og.png"
+				previewBool={true}
+			></Bookmark>
+			<Bookmark
+				title="Hyam"
+				description="Hyam is a product designer at Microsoft ↖︎ focused on next gen artificial intelligence products."
+				href="https://www.hyumankind.com"
+				preview="http://static1.squarespace.com/static/5ec3787ebc8c087ea3382e71/t/5edec0b8c19ff3057554249c/1591656639911/original_4f81e61b229fba04bb60c72cd3bfb2c9.gif?format=1500w"
+				previewBool={true}
+			></Bookmark>
+			<Bookmark
+				title="Arun Venkatesan"
+				description=""
+				href="https://www.hyumankind.com"
+				preview="https://www.arun.is/images/twitter-small.jpg"
+				previewBool={true}
+			></Bookmark>
+			<Bookmark
+				title="Sarah Dayan."
+				description="Sarah Dayan | Staff Software Engineer at Algolia | Creator of Dinero.js | TypeScript, JavaScript, Vue.js, CSS"
+				href="https://sarahdayan.dev"
+				preview="https://sarahdayan.dev/sarah-dayan.jpg"
+				previewBool={true}
+			></Bookmark>
+			<Bookmark
+				title="Rasmus Andersson"
+				description="Ramblings by Rasmus Andersson"
+				href="https://rsms.me"
+				preview=""
+				previewBool={false}
+			></Bookmark>
+			<Bookmark
+				title="Zach Cole"
+				description="Zach Cole is a designer and front-end developer who also loves to run very far."
+				href="http://zcole.me"
+				preview="http://zcole.me/img/social-share.png"
+				previewBool={true}
+			></Bookmark>
+			<Bookmark
+				title="Ben Gold"
+				description=""
+				href="https://ben.gold"
+				preview=""
+				previewBool={false}
+			></Bookmark>
+			<Bookmark
+				title="Ben Gold"
+				description=""
+				href="https://ben.gold"
+				preview=""
+				previewBool={false}
+			></Bookmark>
+			<Bookmark
+				title="Fey"
+				description="Fey provides journaling, chart embedding, data dashboards and secure live trading all in one platform to help you become a more strategic trader."
+				href="https://feyapp.com"
+				preview="https://www.feyapp.com/_next/static/images/meta-home-70ae7c8526660966d13d34dabd6eefa8.jpg"
+				previewBool={true}
+			></Bookmark>
+			<Bookmark
+				title="Stripe"
+				description="Online payment processing for internet businesses."
+				href="https://stripe.com"
+				preview="https://images.ctfassets.net/fzn2n1nzq965/3AGidihOJl4nH9D1vDjM84/9540155d584be52fc54c443b6efa4ae6/homepage.png?fm=jpg"
+				previewBool={true}
+			></Bookmark>
+			<Bookmark
+				title="Vercel"
+				description="Vercel is the easiest way to deploy websites. Host your web projects with zero configuration, automatic SSL, and global CDN."
+				href="https://vercel.com/home"
+				preview="https://assets.vercel.com/image/upload/q_auto/front/vercel/home-2.png"
+				previewBool={true}
+			></Bookmark>
 			<hr></hr>
-			<h2>Tools</h2>
-			<li>https://title.sh/</li>
-			<li>https://static-tweet.now.sh/</li>
-			<li>https://metatags.io/</li>
-			<hr></hr>
-			<h2>Portfolios</h2>
-			<li>https://www.kerem.co</li>
-			<li>https://leo.im</li>
-			<li>https://chibicode.com</li>
-			<li>https://www.hyumankind.com</li>
-			<li>https://www.arun.is</li>
-			<li>https://sarahdayan.dev</li>
-			<li>https://rsms.me</li>
-			<li>http://zcole.me</li>
-			<li>https://ben.gold</li>
-			<li>Products</li>
-			<li>https://feyapp.com/</li>
-			<li>https://stripe.com/</li>
-			<li>https://vercel.com/home</li>
+			<h1>Tools</h1>
+			<p>Tools that I use that make engineering and designing better.</p>
+			<Bookmark
+				title="https://title.sh/"
+				description="Capitalize Your Title"
+				href="https://title.sh/"
+				preview=""
+				previewBool={false}
+			></Bookmark>
+			<Bookmark
+				title="https://static-tweet.now.sh/"
+				description="A demo showing off ahead-of-time and incremental static generation by using Tweets as the datasource"
+				href="https://static-tweet.now.sh"
+				preview="https://static-tweet.now.sh/assets/twitter-card.png"
+				previewBool={true}
+			></Bookmark>
+			<Bookmark
+				title="https://metatags.io/"
+				description="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
+				href="https://metatags.io"
+				preview="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+				previewBool={true}
+			></Bookmark>
 		</Layout>
 	);
 }
